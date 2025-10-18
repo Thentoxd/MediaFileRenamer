17f67204e84df2c83a23451ad3f67b5c962ce3fe
Alex Nicklin <alex@nicklin.name>
2025-10-18 20:20:44 +0100

    First version to get list of files in the current working directory

--- main.cpp
+++ main.cpp
@@ -1,10 +1,35 @@
+#include <filesystem>
+#include <iostream>
 #include <QApplication>
 #include <QPushButton>
+#include <string>
+using namespace std;
 
 int main(int argc, char *argv[]) {
-    QApplication a(argc, argv);
-    QPushButton button("Hello world!", nullptr);
-    button.resize(200, 100);
-    button.show();
-    return QApplication::exec();
+    /* QApplication a(argc, argv);
+    # QPushButton button("Hello world!", nullptr);
+    # button.resize(200, 100);
+    # button.show();
+    # return QApplication::exec(); */
+
+    const std::filesystem::path sandbox{"."};
+
+    // directory_iterator can be iterated using a range-for loop
+    for (auto const& dir_entry : filesystem::directory_iterator{sandbox})
+        if (!dir_entry.is_directory()) {
+            string raw_filename = dir_entry.path().string();
+            raw_filename = raw_filename.substr(2);
+
+            if (raw_filename[0] != '.') {
+                int dot_position = raw_filename.find(".");
+                string final_filename;
+
+                if (dot_position == -1)
+                    final_filename = raw_filename;
+                else {
+                    final_filename = raw_filename.substr(0, dot_position);
+                }
+                std::cout << final_filename << '\n';
+            }
+        }
 }
