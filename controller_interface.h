ad32d8fe27bd03bccc2b67ab356e83392a650d5c
Alex Nicklin <alex@nicklin.name>
2025-10-29 18:46:57 +0000

    Moved model interface

---
+++ controller_interface.h
@@ -0,0 +1,20 @@
+//
+// Created by Alex Nicklin on 29/10/2025.
+//
+
+#ifndef MEDIAFILERENAMER_CONTROLLER_INTERFACE_H
+#define MEDIAFILERENAMER_CONTROLLER_INTERFACE_H
+
+#include <string>
+using namespace std;
+
+class ControllerInterface {
+public:
+    virtual ~ControllerInterface();
+    // virtual void updateCurrentWorkingDirectory(string newCurrentWorkingDirectory) = 0;
+    ///virtual void reload() = 0;
+    // virtual void clear() = 0;
+    //...
+};
+
+#endif //MEDIAFILERENAMER_CONTROLLER_INTERFACE_H
