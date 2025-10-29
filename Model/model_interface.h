4b129892340f65382a0588d9c6d4e975b82ab0f7
Alex Nicklin <alex@nicklin.name>
2025-10-29 18:26:05 +0000

    Added Model interface

---
+++ Model/model_interface.h
@@ -0,0 +1,22 @@
+//
+// Created by Alex Nicklin on 29/10/2025.
+//
+
+
+
+#ifndef MEDIAFILERENAMER_MODEL_INTERFACE_H
+#define MEDIAFILERENAMER_MODEL_INTERFACE_H
+
+#include <string>
+using namespace std;
+
+class model_interface {
+public:
+    virtual ~model_interface();
+    virtual void updateCurrentWorkingDirectory(string newCurrentWorkingDirectory) = 0;
+    virtual void reload() = 0;
+    virtual void clear() = 0;
+    //...
+};
+
+#endif //MEDIAFILERENAMER_MODEL_INTERFACE_H
