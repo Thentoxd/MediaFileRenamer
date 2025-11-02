6ecdadcab6d0a16d32f3335429eb292371165299
thento <alex@nicklin.name>
2025-11-02 11:47:49 +0000

    added getters and setters

--- Model/model.h
+++ Model/model.h
@@ -6,6 +6,7 @@
 #define MEDIAFILERENAMER_MODEL_H
 
 #include <ctime>
+#include <vector>
 
 #include "../model_interface.h"
 
@@ -17,10 +18,73 @@ class FileEntry {
     time_t currentDateTakenOriginal;
     time_t newDateTakenOriginal;
 
+public:
+    FileEntry(string fileName, time_t currentDateTakenOriginal) {
+        this->currentFileName = fileName;
+        this->newFileName = fileName;
+        this->currentDateTakenOriginal = currentDateTakenOriginal;
+        this->newDateTakenOriginal = currentDateTakenOriginal;
+    }
 
+    string getCurrentFileName() {
+        return currentFileName;
+    }
+    string getNewFileName() {
+        return newFileName;
+    }
+    time_t getCurrentDateTakenOriginal() {
+        return currentDateTakenOriginal;
+    }
+    time_t getNewDateTakenOriginal() {
+        return newDateTakenOriginal;
+    }
+
+    void setFileName(string fileName) {
+        currentFileName = fileName;
+        newFileName = fileName;
+    }
+
+    void setNewFileName(string newFileName) {
+        this->newFileName = newFileName;
+    }
+
+    void setDateTaken(time_t dateTaken) {
+        currentDateTakenOriginal = dateTaken;
+        newDateTakenOriginal = dateTaken;
+    }
+
+    void setNewDateTaken(time_t newDateTaken) {
+        newDateTakenOriginal = newDateTaken;
+    }
 };
 
 class FileEntries {
+    vector<FileEntry> fileEntries;
+
+public:
+    void addEntry(FileEntry entry) {
+        fileEntries.push_back(entry);
+    }
+    void addEntry(FileEntry entry, int row) {
+        fileEntries.insert(fileEntries.begin() + row - 1, entry);
+    }
+
+    void removeBackEntry() {
+        fileEntries.pop_back();
+    }
+
+    void removeEntry(int row) {
+        fileEntries.erase(fileEntries.begin() + row - 1);
+    }
+
+    int getEntryCount() {
+        return fileEntries.size();
+    }
+
+    FileEntry getFileEntry(int row) {
+        return fileEntries[row - 1];
+    }
+
 
 };
 
