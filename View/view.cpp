5881e5c7b9aeea27e7c146868e792a476e9b0132
Alex Nicklin <alex@nicklin.name>
2025-11-05 23:35:34 +0000

    Clean-up for grid data

--- View/view.cpp
+++ View/view.cpp
@@ -40,9 +40,6 @@ void mediaFileRenamerMainView::create_window() {
     //    SPDLOG_INFO("Table data (name): {}", currentFileName);
     //}
 
-    int entryCount = p_model -> getEntryCount();
-    SPDLOG_INFO("Number of files in the model: {}", entryCount);
-
     // Disable editing directly
     tableWidget->setEditTriggers(QAbstractItemView::NoEditTriggers);
 
