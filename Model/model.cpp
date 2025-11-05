042baed4a7525d379042ce6746eb0c27e16feff1
Alex Nicklin <alex@nicklin.name>
2025-11-05 16:57:23 +0000

    Moving file info loading to the model class

--- Model/model.cpp
+++ Model/model.cpp
@@ -7,6 +7,8 @@
 
 #include <vector>
 
+#include <filesystem>
+
 using namespace std;
 
 #include "../main.h"
@@ -21,6 +23,41 @@ Model::Model() {
 void Model::setCurrentWorkingDirectory(string newCurrentWorkingDirectory) {
     SPDLOG_INFO("Model: updating current working directory");
     currentWorkingDirectory = newCurrentWorkingDirectory;
+
+    // this -> clear();
+    /*
+    virtual void setFileName(string fileName) = 0;
+    virtual void setNewFileName(string newFileName) = 0;
+    virtual void setDateTaken(time_t dateTaken) = 0;
+    virtual void setNewDateTaken(time_t newDateTaken) = 0;
+    */
+
+    // directory_iterator can be iterated using a range-for loop
+    for (auto const& dir_entry : filesystem::directory_iterator{currentWorkingDirectory}) {
+        if (!dir_entry.is_directory()) {
+            string raw_filename = dir_entry.path().string();
+            raw_filename = raw_filename.substr(2);
+
+            if (raw_filename[0] != '.') {
+                int dot_position = raw_filename.find(".");
+                string final_filename;
+
+                if (dot_position == -1)
+                    final_filename = raw_filename;
+                else {
+                    final_filename = raw_filename.substr(0, dot_position);
+                }
+
+                time_t current_time = time(nullptr);
+
+                // p_model -> addEntry(final_filename, current_time);
+                SPDLOG_INFO("File in CWD: {}", final_filename);
+
+                this -> addEntry(final_filename, current_time);
+            }
+        }
+    }
+    SPDLOG_INFO("New model built");
 }
 
 string Model::getCurrentWorkingDirectory() {
--- main.cpp
+++ main.cpp
@@ -67,40 +67,8 @@ int main(int argc, char *argv[]) {
 
         auto p_model = new Model();
 
-        p_model -> clear();
-
-        /*
-        virtual void setFileName(string fileName) = 0;
-        virtual void setNewFileName(string newFileName) = 0;
-        virtual void setDateTaken(time_t dateTaken) = 0;
-        virtual void setNewDateTaken(time_t newDateTaken) = 0;
-        */
-
-        // directory_iterator can be iterated using a range-for loop
-        for (auto const& dir_entry : filesystem::directory_iterator{current_working_directory})
-            if (!dir_entry.is_directory()) {
-                string raw_filename = dir_entry.path().string();
-                raw_filename = raw_filename.substr(2);
-
-                if (raw_filename[0] != '.') {
-                    int dot_position = raw_filename.find(".");
-                    string final_filename;
-
-                    if (dot_position == -1)
-                        final_filename = raw_filename;
-                    else {
-                        final_filename = raw_filename.substr(0, dot_position);
-                    }
-
-                    time_t current_time = time(nullptr);
-
-                    p_model -> addEntry(final_filename, current_time);
-                    SPDLOG_INFO("File in CWD: {}", final_filename);
-                }
-            }
-
-        // Populate the model
         p_model -> setCurrentWorkingDirectory(current_working_directory.string());
+
 
         auto * p_QApplication = new QApplication(argc, argv);
         auto p_view = new mediaFileRenamerMainView(p_model);
